722f2c99e3e77e3459865fa74edad4a4ebad3c54
Kadet <techkadet@gmail.com>
2020-10-09 23:26:46 +0100

    minor updates

--- src/pages/home/TweetTemplates.js
+++ src/pages/home/TweetTemplates.js
@@ -1,14 +1,14 @@
 export default [
   {
-    message: 'You were elected by human beings to lead, If there are no longer human beings. You won’t be a leader. This is what Sars threatens at the moment. Lend your voice to the great cause and your name shall be written in gold in Nigerian history'
+    message: 'You were elected by human beings to lead, If there are no longer human beings. You won’t be a leader. This is what Sars threatens at the moment. Lend your voice to the great cause and your name shall be written in gold in Nigerian history #ENDSARSNow'
   },
   {
-    message: "please do not pretend that you do not hear the cries of the people you're supposed to lead. If you are still adamant about your ignorance, I am bringing it to your attention. SARS is killing the youths of Nigeria. We want you to do something about it. #ENDSARS now."
+    message: "please do not pretend that you do not hear the cries of the people you're supposed to lead. If you are still adamant about your ignorance, I am bringing it to your attention. SARS is killing the youths of Nigeria. We want you to do something about it. #ENDSARSNow."
   },
   {
     message: "We do not want reformation,  we do not want them to be banned,  we want them ended. #ENDSARSNOW "
   },
   {
-    message: "2017, they were banned, 2018 they were banned again, 2019, they were banned also. Three times is enough for you to know that it is not working #ENDSARSNOW"
+    message: "2017, they were banned, 2018 they were banned again, 2019, they were banned also. Three times is enough for you to know that it is not working #ENDSARSNow"
   }
 ]
