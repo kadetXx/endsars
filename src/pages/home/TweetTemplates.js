47e9f4917d4ce896edfa93e35be27e68bad8903d
Kadet <techkadet@gmail.com>
2020-10-09 23:23:21 +0100

    added more handles

--- src/pages/home/TweetTemplates.js
+++ src/pages/home/TweetTemplates.js
@@ -3,7 +3,7 @@ export default [
     message: 'You were elected by human beings to lead, If there are no longer human beings. You won’t be a leader. This is what Sars threatens at the moment. Lend your voice to the great cause and your name shall be written in gold in Nigerian history'
   },
   {
-    message: "please do not pretend that you do not hear the cries of the people you're supposed to lead. If you are still adamant about your ignorance, I am bringing it to your attention. SARS is killing the youths of Nigeria. We want you to do something about it.  You can do something about it.  #ENDSARS  now."
+    message: "please do not pretend that you do not hear the cries of the people you're supposed to lead. If you are still adamant about your ignorance, I am bringing it to your attention. SARS is killing the youths of Nigeria. We want you to do something about it. #ENDSARS now."
   },
   {
     message: "We do not want reformation,  we do not want them to be banned,  we want them ended. #ENDSARSNOW "
