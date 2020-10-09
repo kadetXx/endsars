c22a8f4fad73d0b741328db00ab5a796ce11350b
Kadet <techkadet@gmail.com>
2020-10-09 19:41:15 +0100

    fixed whatsapp message bug

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,19 +19,19 @@ Young people (our brothers, sisters, friends and your children) have been extort
 
 So they have decided enough is enough. Joined by celebrities such as Folarin “Falz” Falana and Runtown, protests have been holding nationwide since Thursday the 8th of October, still we have no word from the presidency and no coverage from the major media houses. 
 
-We are calling on you our parents because we know the amount of influence you wield to join forces with us to #EndSARS. End SARS not reform it. We’re tired of being extorted because we are trying to make a living, having our laptops taken from us, our phones searched and our accounts being emptied. 
+We are calling on you our parents because we know the amount of influence you wield to join forces with us to EndSARS. End SARS not reform it. We’re tired of being extorted because we are trying to make a living, having our laptops taken from us, our phones searched and our accounts being emptied. 
 
 You can read more about how the protests are going here https://www.zikoko.com/life/citizen/the-police-are-harassing-endsars-protesters-1/
 
 You can also call and email our reps to make them as uncomfortable as possible, you can find their contact details here https://nigeriansenators.firebaseapp.com
 
 Whatever you do, please don’t be silent. 
-Join us to #endsars. 
+Join us to Endsars. 
 
 We are tired of burying our friends, YOU deserve to reap the fruit of your labour over us but that won’t happen if we’re dead. 
 
 Help us by forwarding to all your contacts and taking action 
-*Help us #ENDSARS*
+*Help us ENDSARS*
 
 Yours sincerely 
 A frustrated but hardworking Nigerian youth
