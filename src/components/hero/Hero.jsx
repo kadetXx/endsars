8c938ec3500303983e398154681268d491656e88
Jefferson Osagie Iyobosa <iyobosajefferson2@gmail.com>
2020-10-10 07:47:45 +0100

    Removed CTA button on hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -11,13 +11,7 @@ function Hero( ) {
       <Header transparent={true} />
       <div className={styles.caption} >
         <h2> Stop <Typewriter texts={ crimeKeywords } />!
-          <br/><br/> Let Our Voices be Heard! <br/> <span>#EndSARSNow!</span> </h2>
-          <a 
-            className={ styles.cta } 
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.notion.so/b5632859ff7e4fe09d8cd73880f8aca7?v=6456a7172b9542a0b1487924766eaf7d">
-            Join the protest </a>
+          <br/> Let Our Voices be Heard! <br/> <span>#EndSARSNow!</span> </h2>
       </div>
     </div>
   )
