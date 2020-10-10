88d84e16797b8c7b7e88aa75f6135e7262133c2b
Kadet <techkadet@gmail.com>
2020-10-10 14:00:03 +0100

    added bullets

--- src/pages/home/broadcast.js
+++ src/pages/home/broadcast.js
@@ -6,29 +6,34 @@ The Special Anti robbery Squad is a unit in the Nigerian Police Force originally
 robberies. Unfortunately, officers of the unit who mostly dress in mufti and carry Ak 47 guns in
 unmarked cars have engaged in several cases of kidnapping, murder, theft, rape, torture, illegal
 raids, unlawful arrests, highhandedness, humiliation, unlawful detention and extortion (both
-documented and undocumented). These activities are carried out under the pretext of looking
+documented and undocumented). 
+
+These activities are carried out under the pretext of looking
 for thieves and fraudsters.
 For no reason at all, many innocent Nigerians have been killed by these dangerous police
 officers. Some of them are:
- Remi, killed in Osogbo, September 15 2020
- Tina Ezwkwe, 17 year old girl, killed for standing at a bus stop in Lagos in 2020
- Akinola Lasisi, 33 year old man killed in Sagamu, Ogun State
- Femi Bello, killed in Nasarawa in 2014
- Kolade Johnson, who was killed while watching a football match in Lagos
- Tiamiyu Kazeem, aka Kaka, footballer at Remo Stars FC, killed in Sagamu, Ogun State
- Ifeoma Abugu, raped and murdered in a SARS Station in 2020
- Chibuike Anams, killed while relaxing with a friend
- Christian Ugwuoke, killed while attending his aunt’s wake keep
- Aneka Okorie, killed for not paying bribe
- Ayomide Taiwo, killed for not giving 50 naira bribe
- Godspower Edoha, killed in his home
- These are just some of the hundreds of innocent Nigerian youths killed by SARS officers.
+
+▪️ *Remi*, killed in Osogbo, September 15 2020
+▪️ *Tina Ezwkwe*, 17 year old girl, killed for standing at a bus stop in Lagos in 2020
+▪️ *Akinola Lasisi*, 33 year old man killed in Sagamu, Ogun State
+▪️ *Femi Bello*, killed in Nasarawa in 2014
+▪️ *Kolade Johnson*, who was killed while watching a football match in Lagos
+▪️ *Tiamiyu Kazeem*, aka Kaka, footballer at Remo Stars FC, killed in Sagamu, Ogun State
+▪️ *Ifeoma Abugu*, raped and murdered in a SARS Station in 2020
+▪️ *Chibuike Anams*, killed while relaxing with a friend
+▪️ *Christian Ugwuoke*, killed while attending his aunt’s wake keep
+▪️ *Aneka Okorie*, killed for not paying bribe
+▪️ *Ayomide Taiwo*, killed for not giving 50 naira bribe
+▪️ *Godspower Edoha*, killed in his home
+▪️ These are just some of the hundreds of innocent Nigerian youths killed by SARS officers.
 
 For many years, Nigerians have protested and written petitions for the SRAS unit to be
 scrapped but nothing has changed. After numerous failed promises from the federal
 Government and reforms that did not change anything, young Nigerians are protesting in many
 cities and the protest is attracting international attention. Nigerians in the Diaspora are also
-joining on social media. We need everyone to join us this time around so that this unit can be
+joining on social media. 
+
+We need everyone to join us this time around so that this unit can be
 scrapped once and for all because the officers have proven that they are beyond being
 reformed. We know that the entire force needs to be overhauled but this killer squad must go
 first. Please join us at these protests, encourage us with your voice and money, lobby and talk
