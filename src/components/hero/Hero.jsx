871ac6fc3059dd179576465e63b776b514eb4ac3
Kadet <techkadet@gmail.com>
2020-10-10 11:46:37 +0100

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -10,7 +10,7 @@ function Hero( ) {
     <div className={styles.hero}>
       <Header transparent={true} />
       <div className={styles.caption} >
-        <h2> Stop <Typewriter texts={ crimeKeywords } />!
+        <h2> Stop <Typewriter texts={ crimeKeywords } />
           <br/> Let Our Voices be Heard! <br/> <span>#EndSARSNow!</span> </h2>
         <a className={styles.about} href="https://en.wikipedia.org/wiki/End_SARS" rel="noopener noreferrer" target="_blank">
           About this movement
