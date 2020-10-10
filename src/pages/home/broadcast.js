6858cc9619b823d163f0f89df8b906eddfb7cd04
Kadet <techkadet@gmail.com>
2020-10-10 14:03:38 +0100

    removed paragraphing

--- src/pages/home/broadcast.js
+++ src/pages/home/broadcast.js
@@ -1,17 +1,9 @@
 export default encodeURI(`
 *ATTENTION* ‼️‼️
 
-Why The Special Anti Robbery Squad (SARS) Must be Scrapped
-The Special Anti robbery Squad is a unit in the Nigerian Police Force originally formed to combat
-robberies. Unfortunately, officers of the unit who mostly dress in mufti and carry Ak 47 guns in
-unmarked cars have engaged in several cases of kidnapping, murder, theft, rape, torture, illegal
-raids, unlawful arrests, highhandedness, humiliation, unlawful detention and extortion (both
-documented and undocumented). 
+Why The Special Anti Robbery Squad (SARS) Must be Scrapped. The Special Anti robbery Squad is a unit in the Nigerian Police Force originally formed to combat robberies. Unfortunately, officers of the unit who mostly dress in mufti and carry Ak 47 guns in unmarked cars have engaged in several cases of kidnapping, murder, theft, rape, torture, illegal raids, unlawful arrests, highhandedness, humiliation, unlawful detention and extortion (both documented and undocumented). 
 
-These activities are carried out under the pretext of looking
-for thieves and fraudsters.
-For no reason at all, many innocent Nigerians have been killed by these dangerous police
-officers. Some of them are:
+These activities are carried out under the pretext of looking for thieves and fraudsters. For no reason at all, many innocent Nigerians have been killed by these dangerous police officers. Some of them are:
 
 ▪️ *Remi*, killed in Osogbo, September 15 2020
 ▪️ *Tina Ezwkwe*, 17 year old girl, killed for standing at a bus stop in Lagos in 2020
@@ -27,18 +19,10 @@ officers. Some of them are:
 ▪️ *Godspower Edoha*, killed in his home
 ▪️ These are just some of the hundreds of innocent Nigerian youths killed by SARS officers.
 
-For many years, Nigerians have protested and written petitions for the SRAS unit to be
-scrapped but nothing has changed. After numerous failed promises from the federal
-Government and reforms that did not change anything, young Nigerians are protesting in many
-cities and the protest is attracting international attention. Nigerians in the Diaspora are also
-joining on social media. 
+For many years, Nigerians have protested and written petitions for the SRAS unit to be scrapped but nothing has changed. After numerous failed promises from the federal Government and reforms that did not change anything, young Nigerians are protesting in many cities and the protest is attracting international attention. Nigerians in the Diaspora are also joining on social media. 
 
-We need everyone to join us this time around so that this unit can be
-scrapped once and for all because the officers have proven that they are beyond being
-reformed. We know that the entire force needs to be overhauled but this killer squad must go
-first. Please join us at these protests, encourage us with your voice and money, lobby and talk
-to your friends in power, and release your wards to protest. We are all that we have, our voices
-must be heard.
+We need everyone to join us this time around so that this unit can be scrapped once and for all because the officers have proven that they are beyond being reformed. We know that the entire force needs to be overhauled but this killer squad must go first. Please join us at these protests, encourage us with your voice and money, lobby and talk to your friends in power, and release your wards to protest. We are all that we have, our voices must be heard.
+
 Signed, Millions of Nigerian Youths Protesting
 
 `);
