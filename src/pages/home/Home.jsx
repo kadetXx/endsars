38f92771ac8a35374a38d523dca506bc7dd9aea2
Adedayo Moshood <dharyour93@yahoo.com>
2020-10-12 16:24:41 +0100

    Add #NigeriaBleeds to hashtags

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -32,8 +32,8 @@ function Home() {
     setTweet(encodeURI(fullTweet));
   };
 
-  const hashtags = `SARSMUSTEND,EndSARSNow,EndSARS,EndSARSProtests`;
-  const whatsappTags = `%23SARSMUSTEND %23EndSARSNow %23EndSARSProtests %23EndSARSBrutality`
+  const hashtags = `SARSMUSTEND,EndSARSNow,EndSARS,EndSARSProtests,NIGERIABLEEDS`;
+  const whatsappTags = `%23SARSMUSTEND %23EndSARSNow %23EndSARSProtests %23EndSARSBrutality %23NIGERIABLEEDS`
 
   return (
     <React.Fragment>
