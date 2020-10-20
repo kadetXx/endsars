a67d724900d02197d87af1a6cf18560fc074540a
Kadet <techkadet@gmail.com>
2020-10-20 01:46:48 +0100

    added updated tweets

--- src/pages/home/TweetTemplates.js
+++ src/pages/home/TweetTemplates.js
@@ -1,37 +1,59 @@
 export default [
   {
-    message: 'You were elected by human beings to lead. If there are no longer human beings, You won’t be a leader. This is what SARS threatens at the moment. Lend your voice to the great cause and your name shall be written in gold in Nigerian history'
+    message:
+      "We demand the immediate release of all arrested protesters and also demand the prosecution of all police and SARS officials misconduct NOW!!",
   },
   {
-    message: "please do not pretend that you do not hear the cries of the people you're supposed to lead. I am bringing it to your attention. SARS is killing the youths of Nigeria. We want you to do something about it."
+    message: `Meaningless Nigerian Political Phrases we're tired of hearing
+1. We'll look into it......
+2. We'll set up a committee.....
+3. We condemn.....
+4. It is regrettable.....
+
+We need and demand ACTION!!!`,
   },
   {
-    message: "We do not want reformation,  we do not want them to be banned,  we want them ended."
+    message:
+      "So many lives has been lost to the Awzuku SARS. That unit should be shut down and investigated",
   },
   {
-    message: "2017, they were banned, 2018 they were banned again, 2019, they were banned also. Three times is enough for you to know that it is not working"
+    message: `A reminder that  CSP Nwafor was only sacked and not prosecuted to face justice!
+
+    CSP Nwafor must and should be prosecuted!
+    CSP Nwafor must and should be prosecuted!
+    CSP Nwafor must and should be prosecuted!
+    CSP Nwafor must and should be prosecuted!`,
   },
   {
-    message: "we young ones are tired of being killed, harassed and arrested just for going about with our own gadgets or having money in our bank account, please do something! Use your platform/office to voice out for us"
+    message: `Twelve(12) days onto the protest and no obvious changes has been made?
+    It only proves how unbothered most of you are`,
   },
   {
-    message: "we are being killed and harassed by the same people who are supposed to protect us, please do something, speak for us!"
+    message: `Under the administration of the current IGP and all these shows how incompetent he is!
+
+    The IGP should resign or be sacked!
+    The IGP should resign or be sacked!
+    The IGP should resign or be sacked!`,
   },
   {
-    message: 'we will not back down till our voices are heard, use your office/platform to stand for the youths, we are fighting for the right to be alive which is a basic human right. We want changes!'
+    message: `IGP should resign!
+    IGP should resign
+    IGP should resign`,
   },
   {
-    message: "owning an iPhone isn't a crime, dressing well isn't a crime, being a young person isn't a crime, SARS operatives have done more harm than good, innocent lives are being lost everyday, do something!"
+    message: `You should know we're not protesting for food, electricity, basic amenities and sorts!
+
+    We're protesting to be alive!`,
   },
   {
-    message: "We, the Nigerian youths are in desperate times and we call on you to help us. Lend your voices and amplify our pleas. There would be no customers to patronise you if we are dead"
+    message: `Is your office aware that on quest of a peaceful protest to be alive, we're being killed by those meant to protect us from harm?`,
   },
   {
-    message: "You wouldn't hold political offices without our votes. We voted you into office to serve us. Power resides in the hands of the citizens. Hear our cries"
+    message:
+      "The Nigerian youths are being brutalised, harassed, raped and killed by the police that should be protecting us. We are not safe in our own country. Our cries to the government in power has fallen on deaf ears.",
   },
   {
-    message: "The Nigerian youths are being brutalised, harassed, raped and killed by the police that should be protecting us. We are not safe in our own country. Our cries to the government in power has fallen on deaf ears."
-  },{
-    message: "The structure of the Nigerian police is decayed and needs a total overhaul from top to bottom. The youths are getting killed, raped and brutalised instead of being protected."
-  }
-]
+    message:
+      "The structure of the Nigerian police is decayed and needs a total overhaul from top to bottom. The youths are getting killed, raped and brutalised instead of being protected.",
+  },
+];
